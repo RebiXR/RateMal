dc9472cfcf375f0e1da5147c669d5f1de6deddae
Benjamin Geiger <benjamin.geiger@stud.fh-campuswien.ac.at>
2025-12-29 14:36:05 +0100

    changed socket connection to create a lobby

--- backend/server.ts
+++ backend/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "http";
 import { Server } from "socket.io";
 import cors from "cors";
+import { Socket } from "dgram";
 
 const app = express();
 app.use(cors());
@@ -12,14 +13,22 @@ const io = new Server(httpServer, {
   cors: { origin: "*" },
 });
 
+const LobbyId = "userLobby"
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+    socket.join(LobbyId)
+    socket.to(LobbyId).emit("User connected", socket.id)
+
   socket.on("draw", (data) => {
-    socket.broadcast.emit("draw", data);
+    //socket.broadcast.emit("draw", data);
+
+    socket.to(LobbyId).emit("draw", data)
   });
 
   socket.on("disconnect", () => {
+    socket.to(LobbyId).emit("User disconnected", socket.id)
     console.log("User disconnected:", socket.id);
   });
 });
