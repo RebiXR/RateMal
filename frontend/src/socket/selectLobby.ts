f9242c5a7234c4a205012ca4d2cd0f0374b8ef1c
Benjamin Geiger <benjamin.geiger@stud.fh-campuswien.ac.at>
2026-05-31 19:01:42 +0200

    added/changed lobby socket events

--- frontend/src/socket/selectLobby.ts
+++ frontend/src/socket/selectLobby.ts
@@ -1,15 +1,45 @@
 import { socket } from "./socket";
 
+export type LobbyInfo = {
+  id: string;
+  name: string;
+  isPrivate: boolean;
+  participantCount: number;
+};
+
+export type JoinResponse = { ok: boolean; error?: string };
+export type CreateResponse = { ok: boolean; error?: string; lobbyId?: string };
+
 export const requestLobbies = () => {
   socket.emit("get-lobbies");
 };
 
-export const onLobbyList = (callback: (lobbies: string[]) => void) => {
+export const onLobbyList = (callback: (lobbies: LobbyInfo[]) => void) => {
   socket.on("lobby-list", callback);
 };
 
-export const joinLobby = (lobbyId: string, userId: string) => {
-  socket.emit("join-lobby", lobbyId, userId);
+export const offLobbyList = (callback: (lobbies: LobbyInfo[]) => void) => {
+  socket.off("lobby-list", callback);
+};
+
+export const joinLobby = (
+  lobbyId: string,
+  username: string,
+  password: string | undefined,
+  ack: (res: JoinResponse) => void
+) => {
+  socket.emit("join-lobby", { lobbyId, username, password }, ack);
+};
+
+export const createLobby = (
+  payload: { name: string; isPrivate: boolean; password?: string; username: string },
+  ack: (res: CreateResponse) => void
+) => {
+  socket.emit("create-lobby", payload, ack);
+};
+
+export const leaveLobby = (lobbyId: string) => {
+  socket.emit("leave-lobby", lobbyId);
 };
 
 export const startGuessingGame = (lobbyId: string) => {
